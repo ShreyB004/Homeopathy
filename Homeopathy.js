0a65adf972c1e04a097d30213e6c448849b04edd
Shrey <62979906+ShreyB004@users.noreply.github.com>
2022-09-22 10:00:29 +0530

    Update Homeopathy.js

--- Homeopathy.js
+++ Homeopathy.js
@@ -8,6 +8,12 @@ const firebaseConfig = {
   measurementId: "G-E0GXS6V491"
 };
 
+window.addEventListener("error", function(e){
+	document.getElementById('debug').innerText = `${e.message}`;
+	console.log(e);
+});
+
+
 const fireBaseInit = firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 // const cldStorage = firebase.storage();
